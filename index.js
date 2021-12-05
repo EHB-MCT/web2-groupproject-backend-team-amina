284d31e1e32496e387c17ee912b7495334b2a375
Aminaehb <71768425+Aminaehb@users.noreply.github.com>
2021-12-05 14:09:14 +0100

    push challenges code
    Co-authored-by: niko.brusselaers <niko-brusselaers@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
+const {
+  response
+} = require('express');
 const express = require('express')
+const {
+  MongoClient
+} = require('mongodb');
 const app = express()
 const port = 3000
+const username = "team_amina"
+const password = "thisisourpassword"
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.redirect("info.html")
-})
+//Create the mongo client use
+const uri = `mongodb+srv://<${username}>:<${password}>@cluster0.ruiua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
-app.get('/test', (req, res) => {
-    let data= {
-        name: "widad",
-        age: 19
-    }
-    res.send(data)
-  })
+//Root route
+app.get('/', (req, res) => {
+  //res.status(300).redirect('info.html');
+});
+
+//Return all challenges from db
+app.get('/challenges', async (req, res) => {});
+
+// challenges/:id
+app.get('/challenges/:id', async (req, res) => {});
+
+//save a challenge
+app.post('/challenges', async (req, res) => {});
+
+//update a challenge
+app.put('/challenges/:id', async (req, res) => {
+  response.send('UPDATE OK');
+});
+
+//delete a challenge
+app.delete('/challenges/:id', async (req, res) => {
+  response.send('DELETE OK');
+});
+
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`API running at http://localhost:${port}`)
 })
+
